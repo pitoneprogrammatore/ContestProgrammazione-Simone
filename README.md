c5fada6bd273adc5ac5af91f41e9eb7ae8ea185a
simonegosetto <simone.gosetto@gmail.com>
2021-04-15 17:52:20 +0200

    implementazione script

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # document-analyzer
 Analizzatore Documenti (ITA) creato per il contest https://www.youtube.com/watch?v=CMhOUoeIzyQ
+
+
+// const { GPU } = import('https://cdn.jsdelivr.net/npm/gpu.js@latest/dist/gpu-browser.min.js'); // https://github.com/gpujs/gpu.js/#readme
--- index.js
+++ index.js
@@ -1,28 +1,51 @@
-const tesseract = require('tesseract.js'); // https://github.com/naptha/tesseract.js#tesseractjs
-// https://js.libhunt.com/tesseract-js-alternatives
-const kerasJS = require('keras-js'); //
-const model = new kerasJS.Model({
-    filepath: 'patente.jpg',
-    gpu: true
-})
-/*const scriptJs = require('scriptjs');
-scriptJs('https://cdn.jsdelivr.net/npm/gpu.js@2.11.3/dist/gpu-browser-core.js', (data) => {
-    console.log(data);
-})*/
-// const { GPU } = import('https://cdn.jsdelivr.net/npm/gpu.js@latest/dist/gpu-browser.min.js'); // https://github.com/gpujs/gpu.js/#readme
-
-const tesseractWorker = tesseract.createWorker({
-    logger: m => console.log(m)
-});
-
-const _initTesseract = async () => {
+const jimp = require('jimp');
+const canvas = require('canvas');
+const faceapi = require('face-api.js');
+const faceDetectionNet = faceapi.nets.ssdMobilenetv1;
+const {Canvas, Image, ImageData} = canvas;
+faceapi.env.monkeyPatch({Canvas, Image, ImageData});
+const tesseract = require('tesseract.js');
+const tesseractWorker = tesseract.createWorker();
+const fileToRead = 'https://www.bitmat.it/wp-content/uploads/2018/12/carta_identit%C3%A0_elettronica-696x391.jpg';
+const fileToProcess = 'processed.jpg';
+let documentText, faceData;
+const log = (text) => console.log(`@@@@@@@@@@@@ ${text} @@@@@@@@@@@@`)
+const _init = async () => {
     await tesseractWorker.load();
     await tesseractWorker.loadLanguage('ita');
     await tesseractWorker.initialize('ita');
-    const { data: { text } } = await tesseractWorker.recognize('patente.jpg');
-    console.log(text);
-    await tesseractWorker.terminate();
-};
+    //
+    await faceDetectionNet.loadFromDisk('models');
+    await faceapi.nets.faceLandmark68Net.loadFromDisk('models');
+    await faceapi.nets.ageGenderNet.loadFromDisk('models');
 
-// _initTesseract();
+    documentText = await processOCR();
+    faceData = await processFaceAPI();
+    console.log(`${documentText.toLowerCase().indexOf('paten') > -1 ? log('PATENTE') : documentText.toLowerCase().indexOf('pass') > -1 ? log('PASSAPORTO') : ''}\n${documentText}`);
+    console.log(parseInt(faceData.age), faceData.gender);
+    process.exit(1);
+}
+const processOCR = async () => {
+    const { data: { text } } = await tesseractWorker.recognize(fileToProcess);
+    await tesseractWorker.terminate();
+    return text;
+}
+const processFaceAPI = async () => {
+    const img = await canvas.loadImage(fileToProcess);
+    const faceDetectionOptions = new faceapi.SsdMobilenetv1Options({minConfidence: 0.5}); // getFaceDetectorOptions(faceDetectionNet);
+    const face = await faceapi.detectSingleFace(img, faceDetectionOptions).withFaceLandmarks().withAgeAndGender();
+    return face;
+}
+jimp.read(fileToRead, (err, img) => {
+    if (err) throw err;
+    if (img.bitmap.width > 2000 || img.bitmap.height > 2000) {
+        img.resize(1000, jimp.AUTO).threshold({ max: 200 }).write(fileToProcess);
+    } else {
+        img.threshold({ max: 200 }).write(fileToProcess);
+    }
+    _init();
+});
+
+
+
 
